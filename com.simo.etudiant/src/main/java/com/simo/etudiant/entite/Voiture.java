728974e8f5881474215ba42539f64e75ae050d7d
Simohamed <simo1408@gmail.com>
2018-05-03 16:15:51 -0400

    commit change equal in class voiture

--- com.simo.etudiant/src/main/java/com/simo/etudiant/entite/Voiture.java
+++ com.simo.etudiant/src/main/java/com/simo/etudiant/entite/Voiture.java
@@ -49,4 +49,26 @@ public class Voiture {
                 ", couleur='" + couleur + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Voiture)) return false;
+
+        Voiture voiture = (Voiture) o;
+
+        if (couleur != null ? !couleur.equals(voiture.couleur) : voiture.couleur != null) return false;
+        if (feneter != null ? !feneter.equals(voiture.feneter) : voiture.feneter != null) return false;
+        if (porte != null ? !porte.equals(voiture.porte) : voiture.porte != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = porte != null ? porte.hashCode() : 0;
+        result = 31 * result + (feneter != null ? feneter.hashCode() : 0);
+        result = 31 * result + (couleur != null ? couleur.hashCode() : 0);
+        return result;
+    }
 }
